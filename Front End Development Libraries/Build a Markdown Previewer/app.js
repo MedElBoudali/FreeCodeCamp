65562d5129e51d678b10a5de4a16c2a12b4cca20
MedElBoudali <med.sneaky@gmail.com>
2021-06-27 23:09:14 +0000

    ✨ + app.js

---
+++ Front End Development Libraries/Build a Markdown Previewer/app.js
@@ -0,0 +1,87 @@
+marked.setOptions({
+	breaks: true
+});
+
+const Index = () => {
+	const [data, setData] = React.useState(
+		`# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`
+	);
+
+	const onChange = e => {
+		setData(e.target.value);
+	};
+
+	return (
+		<div id="app-wrapper">
+			<Editor data={data} onChange={onChange} />
+			<Preview data={data} />
+		</div>
+	);
+};
+
+const Editor = ({ data, onChange }) => {
+	return (
+		<div id="editor-wrapper">
+			<h1 class="title">input</h1>
+			<textarea id="editor" type="text" value={data} onChange={onChange}></textarea>
+		</div>
+	);
+};
+
+const Preview = ({ data }) => {
+	return (
+		<div id="preview-wrapper">
+			<h1 class="title">preview</h1>
+			<div
+				id="preview"
+				dangerouslySetInnerHTML={{
+					__html: marked(data)
+				}}></div>
+		</div>
+	);
+};
+
+ReactDOM.render(<Index />, document.getElementById("app"));
